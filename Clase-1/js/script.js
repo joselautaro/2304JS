6ef24ebc1774f216b663d2ceaf91bc8cd567c1ca
Lautaro Montaña <joselautarom@gmail.com>
2024-02-15 22:00:29 -0300

    Clase -1

---
+++ Clase-1/js/script.js
@@ -0,0 +1,90 @@
+// Definición de variable: Espacio reservado de almacenamiento en memoria
+
+// Palabra reservada: palabras que solo se usan en un ambito de lenguaje de programación. 
+
+// Palabras reservadas que hay en js: var, let, const, function, console, reduce, pop, filter,map, string, if, else, switch, for, of, i, prompt, alert etc...
+
+// Tipos de datos primitivos: Array, numéricos, buleanos, string.
+
+
+// Asignación de palabra reservada para variables - asignación de nombre = asignación de dato
+
+// Tipos de variables
+
+// var y let pueden cambiar sus datos a lo largo de la interpretación, const no
+// var edad = 30;
+
+// let direccion = "Paso"
+
+// const nombre = "Lautaro";
+
+// // Depurador por defecto
+// console.log(edad)
+
+// Operadores
+
+// Operadores de asignación: =
+
+// Operadores lógicos: +, -, /, *, ++, --
+
+// Operadores de comparación: 
+
+// ==: igual
+// ===: estrictamente igual (tipo y valor)
+// !=: distinto que
+// !==: estrictamente distinto que
+// >: mayor que
+// <: menor que
+// >=: mayor o igual que
+// <=: menor o igual que
+
+// Operador ternario: ? 
+
+// Operacion de suma
+let primerNumero = 30;
+
+let segundoNumero = 31;
+
+let suma = primerNumero + segundoNumero;
+
+console.log(suma)
+
+// Operacion de resta
+
+let tercerNumero = 80;
+
+let cuartoNumero = 81;
+
+let resta = tercerNumero - cuartoNumero;
+
+console.log(resta);
+
+// Operación de división
+
+let qNumero = 150;
+
+let sNumero = 100;
+
+let division = qNumero / sNumero;
+
+console.log(division)
+
+// ________________________________________________________________________
+
+// Concatenación: es la suma de partes o datos
+
+// let nombre = "Gabriela";
+
+let nombre = prompt("Ingrese su nombre");
+
+// let apellido = "Bustamante";
+
+let apellido = prompt("Ingrese su apellido")
+
+// let espacio = " "; No vale la pena!!!
+
+let info = nombre + " " + apellido;
+
+alert("Te damos la bienvenida: " + info);
+
+// console.log(info)
